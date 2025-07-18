4a78e74aff96ce968f49ab2c997dcfc100bf86b9
André Fonseca <1077309+andreAmorimF@users.noreply.github.com>
2025-07-18 09:59:43 +0200

    Fixing config.el

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -52,7 +52,7 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
-(setq projectile-project-search-path '((getenv "NU_HOME") "~/Workspace/others/cardano")
+(setq projectile-project-search-path '("~/Workspace/nubank" "~/Workspace/others/cardano")
       projectile-enable-caching nil)
 
 ;; Reload buffers when modified on disk
